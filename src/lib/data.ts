405981bd1c19f683c65cdc4d32e459cf0c1100b1
BernydotJar <fearlesed@gmail.com>
2026-07-02 11:40:25 -0600

    Register ConstructHub project data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -5,11 +5,12 @@ import educationData from '@/data/education.json';
 import certificationsData from '@/data/certifications.json';
 import awardsData from '@/data/awards.json';
 import { harnessSdlcProject } from '@/data/harnessProject';
+import { constructHubProject } from '@/data/constructHubProject';
 
 import type { Skill, Project, Experience, Education, Award } from './types';
 
 export const skills: Skill[] = skillsData;
-export const projects: Project[] = [...(projectsData as Project[]), harnessSdlcProject];
+export const projects: Project[] = [...(projectsData as Project[]), harnessSdlcProject, constructHubProject];
 export const experience: Experience[] = experienceData;
 export const education: Education[] = educationData;
 export const certifications: string[] = certificationsData;
